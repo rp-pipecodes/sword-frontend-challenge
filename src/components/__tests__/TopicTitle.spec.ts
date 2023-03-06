d38b06121199512f222cd0a09a6fc54c3f7a5a06
Rafael Passos <rafael.passos@pipecodes.com>
2023-03-06 17:47:27 +0000

    TopicTitle unit tests

--- src/components/__tests__/TopicTitle.spec.ts
+++ src/components/__tests__/TopicTitle.spec.ts
@@ -1,7 +1,7 @@
 import { rest } from "msw";
 import { server } from "./../../setupTests";
 import { describe, it, expect, vi } from "vitest";
-import { flushPromises, shallowMount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import { createTestingPinia } from "@pinia/testing";
 import i18n from "@/i18n";
 import AUTH_MOCK from "./mocks/auth.mock.json";
